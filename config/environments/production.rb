2f4f496a26155a7a437c3d4ef02b32c090033b99
Oleg Sulyanov <oleg@sulyanov.com>
2016-02-19 16:36:22 +0700

    Added Devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.default_url_options = { host: 'geoconfess.herokuapp.com' }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -11,7 +12,7 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
