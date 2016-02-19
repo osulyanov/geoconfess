8f1319b866d657229f905c9f1b264546dc6f5254
Oleg Sulyanov <oleg@sulyanov.com>
2016-02-19 16:53:12 +0700

    Added and configured Active Admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(email: 'admin@example.com', password: '1q2w3e4r', password_confirmation: '1q2w3e4r')
