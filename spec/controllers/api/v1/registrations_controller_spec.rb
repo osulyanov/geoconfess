1ebb2f7d5f969a1d95dba1af2bc08fcb9d5feafd
Oleg Sulyanov <oleg@sulyanov.com>
2016-02-20 17:13:44 +0700

    Improve specs

--- spec/controllers/api/v1/registrations_controller_spec.rb
+++ spec/controllers/api/v1/registrations_controller_spec.rb
@@ -2,18 +2,20 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::RegistrationsController, type: :controller do
   describe 'POST create' do
-    before do
-      post :create, user: attributes_for(:user, name: 'Newbie')
-    end
+    context 'with correct user data' do
+      before do
+        post :create, user: attributes_for(:user, name: 'Newbie')
+      end
 
-    it { expect(response).to have_http_status(:created) }
+      it { expect(response).to have_http_status(:created) }
 
-    it 'creates a new entry' do
-      expect(User.last.name).to eq('Newbie')
-    end
+      it 'creates a new entry' do
+        expect(User.last.name).to eq('Newbie')
+      end
 
-    it 'new user is inactive' do
-      expect(User.last.active?).to be false
+      it 'new user is inactive' do
+        expect(User.last).not_to be_active
+      end
     end
   end
 end
