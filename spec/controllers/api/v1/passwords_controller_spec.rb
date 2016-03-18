d8c03ea02553c11f830aab7846612aca42781f88
Oleg Sulyanov <oleg@sulyanov.com>
2016-03-18 10:14:31 +0700

    Added tests for Passwords controller

---
+++ spec/controllers/api/v1/passwords_controller_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::PasswordsController, type: :controller do
+
+  describe 'POST #create' do
+    let!(:user) { create :user }
+
+    context 'with email of existing user' do
+      it 'returns http success' do
+        post :create, email: user.email
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'sends an email' do
+        expect {
+          post :create, email: user.email
+        }.to change { ActionMailer::Base.deliveries.count }.by(1)
+      end
+    end
+
+    context 'with wrong email' do
+      it 'returns not found' do
+        post :create, email: 'wrong@mail.ru'
+        expect(response).to have_http_status(:not_found)
+      end
+
+      it 'does\'n send an email' do
+        expect {
+          post :create, email: 'wrong@mail.ru'
+        }.to_not change { ActionMailer::Base.deliveries.count }
+      end
+    end
+  end
+
+end
