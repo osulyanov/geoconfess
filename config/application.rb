3a2e78cf5b66885a923a42b750ec260eaa42b8bd
Oleg Sulyanov <oleg@sulyanov.com>
2016-03-19 01:08:13 +0700

    Configure postmark

--- config/application.rb
+++ config/application.rb
@@ -21,8 +21,6 @@ module Geoconfess
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.action_mailer.postmark_settings = { :api_token => Rails.application.secrets.postmark_api_key }
-    config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
-    config.action_mailer.default_options = { :from => "geoconfess@angeltech.io" }
+    config.action_mailer.default_options = { :from => 'geoconfess@angeltech.io' }
   end
 end
