434784445977a1ef289cd08b4137a611de57c025
Oleg Sulyanov <oleg@sulyanov.com>
2016-03-18 10:27:46 +0700

    Added docs

--- app/controllers/api/v1/passwords_controller.rb
+++ app/controllers/api/v1/passwords_controller.rb
@@ -1,4 +1,21 @@
 class Api::V1::PasswordsController < Api::V1::V1Controller
+
+  resource_description do
+    short 'User Password'
+  end
+
+  api :POST, '/create', 'Send reset password instructions'
+  description <<-EOS
+    ## Description
+    Sends reset password instruction on given email address.
+    Returns code 201 if passwords instructions email successfully sent.
+    Returns code 404 if user with given email doesn't exist.
+    Returns code 400 if email doesn't present.
+  EOS
+  param :user, Hash, desc: 'User info', required: true do
+    param :email, String, desc: 'Email', required: true
+  end
+
   def create
     user = User.find_by(email: user_params)
     if user.present?
@@ -12,6 +29,6 @@ class Api::V1::PasswordsController < Api::V1::V1Controller
   private
 
   def user_params
-    params.require(:email)
+    params.require(:user).require(:email)
   end
 end
--- spec/controllers/api/v1/passwords_controller_spec.rb
+++ spec/controllers/api/v1/passwords_controller_spec.rb
@@ -7,26 +7,26 @@ RSpec.describe Api::V1::PasswordsController, type: :controller do
 
     context 'with email of existing user' do
       it 'returns http success' do
-        post :create, email: user.email
+        post :create, user: { email: user.email }
         expect(response).to have_http_status(:success)
       end
 
       it 'sends an email' do
         expect {
-          post :create, email: user.email
+          post :create, user: { email: user.email }
         }.to change { ActionMailer::Base.deliveries.count }.by(1)
       end
     end
 
     context 'with wrong email' do
       it 'returns not found' do
-        post :create, email: 'wrong@mail.ru'
+        post :create, user: { email: 'wrong@mail.ru' }
         expect(response).to have_http_status(:not_found)
       end
 
       it 'does\'n send an email' do
         expect {
-          post :create, email: 'wrong@mail.ru'
+          post :create, user: { email: 'wrong@mail.ru' }
         }.to_not change { ActionMailer::Base.deliveries.count }
       end
     end
