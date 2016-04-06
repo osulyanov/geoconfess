6bdcd863fd65c7543d938c05369cbd9be0d72f66
Oleg Sulyanov <oleg@sulyanov.com>
2016-04-06 17:42:40 +0700

    Created notification factory

---
+++ spec/factories/notifications.rb
@@ -0,0 +1,23 @@
+FactoryGirl.define do
+  factory :notification do
+    unread true
+  end
+end
+
+# == Schema Information
+#
+# Table name: notifications
+#
+#  id                    :integer          not null, primary key
+#  user_id               :integer
+#  notificationable_id   :integer
+#  notificationable_type :string
+#  unread                :boolean          default(TRUE), not null
+#  created_at            :datetime         not null
+#  updated_at            :datetime         not null
+#
+# Indexes
+#
+#  index_notifications_on_user_id                   (user_id)
+#  index_notifs_on_notifable_type_and_notifable_id  (notificationable_type,notificationable_id)
+#
