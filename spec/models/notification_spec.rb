d3a36f3a3d58de1fe5eca9c6f1f7a01db82e715f
Oleg Sulyanov <oleg@sulyanov.com>
2016-04-07 14:02:27 +0700

    Cover Notification model with tests

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -23,6 +23,61 @@ RSpec.describe Notification, type: :model do
   it 'unread by default' do
     expect(subject).to be_unread
   end
+
+  describe '.unread' do
+    let! (:unread) { create(:notification, user: recipient, notificationable: message) }
+    let! (:read) { create(:notification, user: recipient, notificationable: message, unread: false) }
+    subject { Notification.unread }
+
+    it 'returns unread notification' do
+      expect(subject).to include(unread)
+    end
+
+    it 'returns unread notification' do
+      expect(subject).not_to include(read)
+    end
+  end
+
+  describe '.actual' do
+    before do
+      250.times do
+        create(:notification, user: recipient, notificationable: message, created_at: rand(60).days.ago)
+      end
+    end
+    subject { Notification.actual }
+
+    it 'returns no more than 99 notification' do
+      expect(subject.size).not_to be > 99
+    end
+
+    it 'returns unread notification' do
+      expect(subject.last.created_at).not_to be > 1.month.ago
+    end
+  end
+
+  describe '#set_read' do
+    subject { create(:notification, user: recipient, notificationable: message) }
+    before { subject.set_read }
+
+    it 'set unread to false' do
+      expect(subject).not_to be_unread
+    end
+
+    it 'doesn\'t save changes' do
+      subject.reload
+      expect(subject).to be_unread
+    end
+  end
+
+  describe '#set_read!' do
+    subject { create(:notification, user: recipient, notificationable: message) }
+    before { subject.set_read! }
+
+    it 'set unread to false and save changes' do
+      subject.reload
+      expect(subject).not_to be_unread
+    end
+  end
 end
 
 # == Schema Information
