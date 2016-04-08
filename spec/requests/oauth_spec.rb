d468ba32c5c74741b280174dec3747e49692a426
Oleg Sulyanov <oleg@sulyanov.com>
2016-04-08 15:27:21 +0700

    Add tests for OAuth: check that os and push token are changed

--- spec/requests/oauth_spec.rb
+++ spec/requests/oauth_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'OAuth authorization', type: :request do
-  let(:user) { create(:user) }
+  let(:user) { create(:user, os: nil, push_token: nil) }
   let(:client) do
     OAuth2::Client.new('', '') do |b|
       b.request :url_encoded
@@ -13,6 +13,20 @@ RSpec.describe 'OAuth authorization', type: :request do
     it 'auth ok' do
       token = client.password.get_token(user.email, user.password)
       expect(token).not_to be_expired
+    end
+
+    it 'updates user\'s os and push token' do
+      client.password.get_token(user.email, user.password,
+                                os: 'android', push_token: '123456')
+      user.reload
+      expect(user.os).to eq('android')
+    end
+
+    it 'updates user\'s push token' do
+      client.password.get_token(user.email, user.password,
+                                os: 'android', push_token: '123456')
+      user.reload
+      expect(user.push_token).to eq('123456')
     end
   end
 
