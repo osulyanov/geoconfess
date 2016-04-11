26017bd155b2180370a940b467500147e00b6b53
Oleg Sulyanov <oleg@sulyanov.com>
2016-04-11 21:27:19 +0700

    Send push in AskPriestJob

--- app/jobs/ask_priest_job.rb
+++ app/jobs/ask_priest_job.rb
@@ -1,9 +1,20 @@
 class AskPriestJob
   @queue = :simple
 
-  def self.perform(str)
-    # …
-    Rails.logger.info "Job is done! #{str}"
-    puts "Job is done! #{str}"
+  def self.perform(recurrence_id)
+    Rails.logger.info "AskPriestJob #{recurrence_id}"
+
+    recurrence = Recurrence.includes(:spot, spot: [:priest]).find(recurrence_id)
+    spot = recurrence.spot
+
+    PushService.new({
+                      user: spot.priest,
+                      text: 'Confirmez votre disponibilité pour confesser!',
+                      aps: {
+                        model: 'Recurrence',
+                        id: recurrence_id,
+                        action: 'availability'
+                      }
+                    }).push!
   end
 end
