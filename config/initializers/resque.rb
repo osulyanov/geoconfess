b65f07e0e74799511c6cdd99f715ad83585220de
Oleg Sulyanov <oleg@sulyanov.com>
2016-04-11 16:37:10 +0700

    Use REDISTOGO_URL on Heroku

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,3 +1,5 @@
-uri = URI.parse(ENV['REMOTE_REDIS_URL'])
-Resque.redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
+if ENV['REDISTOGO_URL']
+  uri = URI.parse(ENV['REDISTOGO_URL'])
+  Resque.redis = Redis.new(host: uri.host, port: uri.port, password: uri.password, thread_safe: true)
+end
 Dir["#{Rails.root}/app/jobs/*.rb"].each { |file| require file }
