67e9af74797247b1be41c4cf994182411d38bca8
Oleg Sulyanov <oleg@sulyanov.com>
2016-04-11 22:16:46 +0700

    Create a job after recurrence create

--- app/models/concerns/askable.rb
+++ app/models/concerns/askable.rb
@@ -4,11 +4,16 @@ module Askable
   included do
     before_update :remove_old_job
     before_destroy :remove_old_job
-
+    after_create :create_job
   end
 
   def remove_old_job
     Resque.remove_delayed(AskPriestJob, id)
+  end
+
+  def create_job
+    return unless
+    Resque.enqueue_at(5.seconds.from_now, AskPriestJob, id)
   end
 
   module ClassMethods
