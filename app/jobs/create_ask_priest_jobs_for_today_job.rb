fc2b6ddf86beae89ca201ad90c9f93b6b20ac6d5
Oleg Sulyanov <oleg@sulyanov.com>
2016-04-12 11:33:36 +0700

    Fix job which creates the jobs for today

--- app/jobs/create_ask_priest_jobs_for_today_job.rb
+++ app/jobs/create_ask_priest_jobs_for_today_job.rb
@@ -4,10 +4,10 @@ class CreateAskPriestJobsForTodayJob
   def self.perform
     today = Time.zone.today
     Rails.logger.info "CreateAskPriestJobsForTodayJob #{today}"
-    # recurrences = Recurrence.where('recurrences.date = ? OR recurrences.date ISNULL', today)
-    # recurrences.each do |recurrence|
-    #   return unless recurrence.today?
-    #   recurrence.update_job
-    # end
+    recurrences = Recurrence.where('recurrences.date = ? OR recurrences.date ISNULL', today)
+    recurrences.each do |recurrence|
+      next unless recurrence.today?
+      recurrence.update_job
+    end
   end
 end
