7db02c6d91348e6793438202293f885979ed3ae0
Oleg Sulyanov <oleg@sulyanov.com>
2016-04-14 13:45:05 +0700

    Create favorite model with relations and validations

---
+++ app/models/favorite.rb
@@ -0,0 +1,21 @@
+class Favorite < ActiveRecord::Base
+  belongs_to :user, required: true
+  belongs_to :priest, class_name: 'User', foreign_key: 'priest_id',
+             inverse_of: 'favorites', required: true
+end
+
+# == Schema Information
+#
+# Table name: favorites
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer
+#  priest_id  :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_favorites_on_priest_id  (priest_id)
+#  index_favorites_on_user_id    (user_id)
+#
