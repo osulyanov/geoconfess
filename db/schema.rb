205a2e98f6a8a468c246f9f048996560c5af8c27
Oleg Sulyanov <oleg@sulyanov.com>
2016-04-14 13:45:19 +0700

    Create favorites table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160412050316) do
+ActiveRecord::Schema.define(version: 20160414054207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,16 @@ ActiveRecord::Schema.define(version: 20160412050316) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "priest_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "favorites", ["priest_id"], name: "index_favorites_on_priest_id", using: :btree
+  add_index "favorites", ["user_id"], name: "index_favorites_on_user_id", using: :btree
 
   create_table "meet_requests", force: :cascade do |t|
     t.integer  "priest_id"
