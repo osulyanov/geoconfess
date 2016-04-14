ff13bbae43b28fc2f8491b1f3dda0be6cf67e6f2
Oleg Sulyanov <oleg@sulyanov.com>
2016-04-14 14:23:28 +0700

    Annotate models

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -19,3 +19,19 @@ RSpec.describe Favorite, type: :model do
     expect(subject).not_to be_valid
   end
 end
+
+# == Schema Information
+#
+# Table name: favorites
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer
+#  priest_id  :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_favorites_on_priest_id  (priest_id)
+#  index_favorites_on_user_id    (user_id)
+#
