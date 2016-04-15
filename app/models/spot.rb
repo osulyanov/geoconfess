fe26ee86bb9fed0ed726658819038b2fe5003ae4
Oleg Sulyanov <oleg@sulyanov.com>
2016-04-15 14:10:38 +0700

    Add Spot.outdated scope

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -3,7 +3,8 @@ class Spot < ActiveRecord::Base
 
   enum activity_type: [:static, :dynamic]
 
-  belongs_to :priest, class_name: 'User', foreign_key: 'priest_id', inverse_of: 'spots', required: true
+  belongs_to :priest, class_name: 'User', foreign_key: 'priest_id',
+             inverse_of: 'spots', required: true
   belongs_to :church, dependent: :destroy
   has_many :recurrences, dependent: :destroy
 
@@ -16,30 +17,34 @@ class Spot < ActiveRecord::Base
     where('spots.priest_id IN (SELECT id FROM users WHERE users.active IS TRUE)')
   }
   scope :now, lambda {
-    where('spots.activity_type = ? OR spots.id IN (?)', Spot.activity_types[:dynamic],
+    where('spots.activity_type = ? OR spots.id IN (?)', activity_types[:dynamic],
           Recurrence.now.map { |r| r[:spot_id] })
   }
   scope :of_priest, lambda { |priest_id|
     where('spots.priest_id = ? ', priest_id.to_i)
   }
   scope :of_type, lambda { |type|
-    where('spots.activity_type = ? ', Spot.activity_types[type.to_s])
+    where('spots.activity_type = ? ', activity_types[type.to_s])
   }
   scope :nearest, lambda { |lat, lng, distance|
     near([lat, lng], distance, order: 'distance', units: :km)
+  }
+  scope :outdated, lambda {
+    where('spots.activity_type = ?', activity_types[:dynamic]).
+      where('spots.updated_at < NOW() - \'15 minutes\'::INTERVAL')
   }
 
   before_validation :cache_coordinates
 
   def self.find_or_new(params)
-    dynamic_id = Spot.activity_types[:dynamic]
+    dynamic_id = activity_types[:dynamic]
     spot = find_by(activity_type: dynamic_id) if params[:activity_type] == 'dynamic'
     spot ||= new(spot_params)
   end
 
   def active_today?
     dynamic? || recurrences.select do |r|
-      r.date ==Time.zone.today || r.week_days_arr[Time.zone.today.wday] == 1
+      r.date == Time.zone.today || r.week_days_arr[Time.zone.today.wday] == 1
     end.any?
   end
 
