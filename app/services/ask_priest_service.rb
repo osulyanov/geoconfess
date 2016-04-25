37a4b2f4763dae146204dac1fa8e9416ba8f6795
Oleg Sulyanov <oleg@sulyanov.com>
2016-04-25 16:52:36 +0700

    Add AskPriestService

---
+++ app/services/ask_priest_service.rb
@@ -0,0 +1,40 @@
+class AskPriestService
+  def initialize(recurrence_id)
+    @recurrence = Recurrence.includes(:spot, spot: [:priest]).find(recurrence_id)
+    @spot = @recurrence.spot
+  end
+
+  def notify
+    return unless destroy_if_old
+
+    increase_busy_counter
+
+    create_push
+  end
+
+  # Check if priest doesn't available for 3 times before that
+  def destroy_if_old
+    if @recurrence.busy_count >= 3
+      # Completely remove the recurrence
+      @recurrence.destroy
+      return true
+    end
+  end
+
+  # Increase busy counter
+  def increase_busy_counter
+    @recurrence.increment! :busy_count
+  end
+
+  def create_push
+    PushService.new({
+                      user: @spot.priest,
+                      text: 'Confirmez votre disponibilité pour confesser!',
+                      aps: {
+                        model: 'Recurrence',
+                        id: @recurrence_id,
+                        action: 'availability'
+                      }
+                    }).push!
+  end
+end
