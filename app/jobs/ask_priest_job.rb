c91b48c7c2f8246bda0746559921c4ed5fd9ace1
Oleg Sulyanov <oleg@sulyanov.com>
2016-04-25 16:52:49 +0700

    Refactor AskPriestJob

--- app/jobs/ask_priest_job.rb
+++ app/jobs/ask_priest_job.rb
@@ -2,27 +2,6 @@ class AskPriestJob < ActiveJob::Base
   queue_as :default
 
   def perform(recurrence_id)
-    recurrence = Recurrence.includes(:spot, spot: [:priest]).find(recurrence_id)
-    spot = recurrence.spot
-
-    # Check if priest doesn't available for 3 times before that
-    if recurrence.busy_count >= 3
-      # Completely remove the recurrence
-      recurrence.destroy
-      return
-    end
-
-    # Increase busy counter
-    recurrence.increment! :busy_count
-
-    PushService.new({
-                      user: spot.priest,
-                      text: 'Confirmez votre disponibilité pour confesser!',
-                      aps: {
-                        model: 'Recurrence',
-                        id: recurrence_id,
-                        action: 'availability'
-                      }
-                    }).push!
+    AskPriestService.new(recurrence_id).notify
   end
 end
