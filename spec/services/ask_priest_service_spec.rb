1cead3663557cb31c8e38d48b143081859133be8
Oleg Sulyanov <oleg@sulyanov.com>
2016-04-25 19:26:29 +0700

    Add tests for negative cases of AskPriestService#destroy_if_old

--- spec/services/ask_priest_service_spec.rb
+++ spec/services/ask_priest_service_spec.rb
@@ -12,7 +12,8 @@ describe AskPriestService do
     subject { AskPriestService.new }
 
     it 'handles argument error' do
-      expect { subject }.to raise_error(ArgumentError, 'wrong number of arguments (0 for 1)')
+      expect { subject }
+        .to raise_error(ArgumentError, 'wrong number of arguments (0 for 1)')
     end
   end
 
@@ -50,6 +51,27 @@ describe AskPriestService do
         result = subject.is_inactive
 
         expect(result).to be true
+      end
+    end
+  end
+
+  describe '#destroy_if_old' do
+    context 'recurrence is inactive' do
+      let!(:recurrence) { create(:recurrence, spot: spot, date: Time.zone.today,
+                                 start_at: '14:00', stop_at: '15:00',
+                                 busy_count: 4) }
+
+      it 'return false' do
+        result = subject.destroy_if_old
+
+        expect(result).to be true
+      end
+
+      it 'doesn\'t remove recurrence' do
+        subject.destroy_if_old
+
+        expect { recurrence.reload }
+          .to raise_error(ActiveRecord::RecordNotFound)
       end
     end
   end
