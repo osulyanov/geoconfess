f85a76509cee4704cb54eff1d4ea907e4803de43
Oleg Sulyanov <oleg@sulyanov.com>
2016-04-27 16:52:24 +0700

    Add tests for ChatsController

---
+++ spec/controllers/api/v1/chats_controller_spec.rb
@@ -0,0 +1,72 @@
+require 'rails_helper'
+
+describe Api::V1::ChatsController, type: :controller do
+
+  let(:user) { create :user }
+  let(:sender) { create :user }
+  let(:recipient) { create :user }
+  let(:other_sender) { create :user }
+  let(:other_recipient) { create :user }
+  let!(:message_in_1) { create :message, sender: sender, recipient: user, created_at: 1.second.ago }
+  let!(:message_in_2) { create :message, sender: sender, recipient: user, created_at: 5.seconds.ago }
+  let!(:message_out_1) { create :message, sender: user, recipient: recipient, created_at: 4.second.ago }
+  let!(:message_out_2) { create :message, sender: user, recipient: recipient, created_at: 3.second.ago }
+  let!(:other_message) { create :message, sender: other_sender, recipient: other_recipient }
+
+  describe 'GET #index' do
+    let(:token) { create :access_token, resource_owner_id: user.id }
+
+    before do
+      get :index, format: :json, access_token: token.token
+    end
+
+    it { expect(response).to have_http_status(:success) }
+
+    it 'returns chats for current user as json' do
+      result = json.map { |r| r['id'] }
+
+      expect(result).to contain_exactly(sender.id, recipient.id)
+    end
+
+    context 'with expired access_token' do
+      let (:token) { create :access_token, resource_owner_id: sender.id, expires_in: 0 }
+
+      it { expect(response).to have_http_status(:unauthorized) }
+
+      it { expect(response.body).to be_empty }
+    end
+  end
+
+
+  describe 'GET #show' do
+    let(:token) { create :access_token, resource_owner_id: user.id }
+    context 'for current user' do
+
+      before do
+        get :show, format: :json, id: recipient.id, access_token: token.token
+      end
+
+      it { expect(response).to have_http_status(:success) }
+
+      it 'returns message with certain user as json' do
+        result = json.map { |r| r['id'] }
+
+        expect(result).to contain_exactly(message_out_2.id, message_out_1.id)
+      end
+    end
+
+    context 'for other_user' do
+      let(:token) { create :access_token, resource_owner_id: sender.id }
+
+      before do
+        get :show, format: :json, id: other_message.id, access_token: token.token
+      end
+
+      it 'returns empty result' do
+        result = json
+
+        expect(result).to eq([])
+      end
+    end
+  end
+end
