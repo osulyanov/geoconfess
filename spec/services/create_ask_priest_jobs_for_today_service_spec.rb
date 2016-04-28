083d178efd6bbe27b6343094897ab4a5c53305ae
Oleg Sulyanov <oleg@sulyanov.com>
2016-04-28 16:11:56 +0700

    Add tests for CreateAskPriestJobsForTodayService#process_recurrence

--- spec/services/create_ask_priest_jobs_for_today_service_spec.rb
+++ spec/services/create_ask_priest_jobs_for_today_service_spec.rb
@@ -24,4 +24,32 @@ describe CreateAskPriestJobsForTodayService do
       expect(result).not_to include(tomorrow_recurrence)
     end
   end
+
+  describe '#process_recurrence' do
+    context 'recurrence doesn\'t happen today' do
+      it 'returns true' do
+        result = subject.process_recurrence(tomorrow_recurrence)
+
+        expect(result).to be true
+      end
+
+      it 'doesn\'t call #update_job' do
+        allow(tomorrow_recurrence).to receive(:update_job)
+
+        subject.process_recurrence(tomorrow_recurrence)
+
+        expect(tomorrow_recurrence).not_to have_received(:update_job)
+      end
+    end
+
+    context 'recurrence happens today' do
+      it 'calls #update_job' do
+        allow(today_recurrence).to receive(:update_job)
+
+        subject.process_recurrence(today_recurrence)
+
+        expect(today_recurrence).to have_received(:update_job)
+      end
+    end
+  end
 end
