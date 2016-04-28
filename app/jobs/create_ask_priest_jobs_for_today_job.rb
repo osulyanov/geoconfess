ec548a26f81a806c399c8b6fc601c1cd3de97cd8
Oleg Sulyanov <oleg@sulyanov.com>
2016-04-28 15:58:17 +0700

    Extract code from CreateAskPriestJobsForTodayJob to the service

--- app/jobs/create_ask_priest_jobs_for_today_job.rb
+++ app/jobs/create_ask_priest_jobs_for_today_job.rb
@@ -2,11 +2,6 @@ class CreateAskPriestJobsForTodayJob < ActiveJob::Base
   queue_as :default
 
   def perform
-    today = Time.zone.today
-    recurrences = Recurrence.where('recurrences.date = ? OR recurrences.date ISNULL', today)
-    recurrences.each do |recurrence|
-      next unless recurrence.today?
-      recurrence.update_job
-    end
+    CreateAskPriestJobsForTodayService.new.process
   end
 end
