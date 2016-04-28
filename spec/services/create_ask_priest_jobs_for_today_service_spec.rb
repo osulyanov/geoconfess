35d4d4a40726e5261e0e290340ef14b23f63ad27
Oleg Sulyanov <oleg@sulyanov.com>
2016-04-28 16:36:01 +0700

    Add tests for CreateAskPriestJobsForTodayService#process

--- spec/services/create_ask_priest_jobs_for_today_service_spec.rb
+++ spec/services/create_ask_priest_jobs_for_today_service_spec.rb
@@ -4,8 +4,12 @@ describe CreateAskPriestJobsForTodayService do
 
   let(:priest) { create(:user, role: :priest) }
   let(:spot) { create(:spot, priest: priest) }
+  let(:wdays) { Date::DAYNAMES - [Time.zone.today.strftime('%A')] }
   let!(:today_recurrence) { create(:recurrence, spot: spot, date: Time.zone.today,
                                    start_at: '14:00', stop_at: '15:00') }
+  let!(:recurred_recurrence) { create(:recurrence, spot: spot, date: nil,
+                                      start_at: '14:00', stop_at: '15:00',
+                                      week_days: wdays) }
   let!(:tomorrow_recurrence) { create(:recurrence, spot: spot, date: 1.day.from_now,
                                       start_at: '14:00', stop_at: '15:00') }
 
@@ -18,6 +22,12 @@ describe CreateAskPriestJobsForTodayService do
       expect(result).to include(today_recurrence)
     end
 
+    it 'returns recurred recurrences' do
+      result = subject.todays_recurrences
+
+      expect(result).to include(recurred_recurrence)
+    end
+
     it 'doesn\'t return recurrences of other days' do
       result = subject.todays_recurrences
 
@@ -28,17 +38,17 @@ describe CreateAskPriestJobsForTodayService do
   describe '#process_recurrence' do
     context 'recurrence doesn\'t happen today' do
       it 'returns true' do
-        result = subject.process_recurrence(tomorrow_recurrence)
+        result = subject.process_recurrence(recurred_recurrence)
 
         expect(result).to be true
       end
 
       it 'doesn\'t call #update_job' do
-        allow(tomorrow_recurrence).to receive(:update_job)
+        allow(recurred_recurrence).to receive(:update_job)
 
-        subject.process_recurrence(tomorrow_recurrence)
+        subject.process_recurrence(recurred_recurrence)
 
-        expect(tomorrow_recurrence).not_to have_received(:update_job)
+        expect(recurred_recurrence).not_to have_received(:update_job)
       end
     end
 
@@ -50,6 +60,17 @@ describe CreateAskPriestJobsForTodayService do
 
         expect(today_recurrence).to have_received(:update_job)
       end
+    end
+  end
+
+  describe '#process' do
+    it 'calls #process_recurrence for each recurrence' do
+      allow(subject).to receive(:process_recurrence)
+
+      subject.process
+
+      expect(subject).to have_received(:process_recurrence).with(today_recurrence)
+      expect(subject).to have_received(:process_recurrence).with(recurred_recurrence)
     end
   end
 end
