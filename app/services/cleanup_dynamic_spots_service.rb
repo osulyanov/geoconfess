cee43f10d34eb7d8b7c81ad1de7d21d9a72978fa
Oleg Sulyanov <oleg@sulyanov.com>
2016-05-02 18:15:57 +0700

    Refactor CleanupDynamicSpotsService to make the code more testable

--- app/services/cleanup_dynamic_spots_service.rb
+++ app/services/cleanup_dynamic_spots_service.rb
@@ -1,9 +1,13 @@
 class CleanupDynamicSpotsService
   def initialize
-    @outdated_spots = Spot.outdated
+    @outdated_spots = spots_to_remove
   end
 
   def perform
     @outdated_spots.destroy_all
+  end
+
+  def spots_to_remove
+    Spot.outdated
   end
 end
