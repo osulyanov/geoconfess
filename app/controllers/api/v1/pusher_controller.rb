611c874ebcc767d51f986c108a4c37191e7beb95
Oleg Sulyanov <oleg@sulyanov.com>
2016-05-02 19:55:02 +0700

    Add docs to PusherController API

--- app/controllers/api/v1/pusher_controller.rb
+++ app/controllers/api/v1/pusher_controller.rb
@@ -1,6 +1,17 @@
 class Api::V1::PusherController < Api::V1::V1Controller
   before_action :doorkeeper_authorize!
 
+  resource_description do
+    short 'Pusher'
+  end
+
+  api :POST, '/v1/pusher/aouth', 'Authenticate subscription'
+  description <<-EOS
+    ## Description
+    Authenticate subscription requests to private channel
+  EOS
+  param :channel_name, String, desc: 'Channel name, as "private-#{current_user.id}"', required: true
+
   def auth
     unless params[:channel_name] == "private-#{current_user.id}"
       head :forbidden and return
