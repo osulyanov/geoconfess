0dba145617edae7e9524d3e0a166110f036aec82
Oleg Sulyanov <oleg@sulyanov.com>
2016-05-03 08:46:27 +0700

    Add test for PusherController#uath API without channel name

--- spec/controllers/api/v1/pusher_controller_spec.rb
+++ spec/controllers/api/v1/pusher_controller_spec.rb
@@ -6,7 +6,16 @@ describe Api::V1::PusherController, type: :controller do
   let (:token) { create :access_token, resource_owner_id: user.id }
 
   describe 'POST #auth' do
+    context 'without channel_name' do
+      before do
+        post :auth, format: :json, socket_id: '4cacr23qd423',
+             access_token: token.token
+      end
 
+      it { expect(response).to have_http_status(:forbidden) }
+
+      it { expect(response.body).to be_empty }
+    end
 
     context 'with expired access_token' do
       let (:token) { create :access_token, resource_owner_id: user.id, expires_in: 0 }
