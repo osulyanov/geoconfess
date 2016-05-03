0eaeff59553ab75edd798daa58cee198ac7f33d9
Oleg Sulyanov <oleg@sulyanov.com>
2016-05-03 09:34:47 +0700

    Update tests for RegistrationsController API

--- spec/controllers/api/v1/registrations_controller_spec.rb
+++ spec/controllers/api/v1/registrations_controller_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe Api::V1::RegistrationsController, type: :controller do
+describe Api::V1::RegistrationsController, type: :controller do
   describe 'POST create' do
     context 'with correct user data' do
       let(:user_data) { attributes_for(:user, name: 'Newbie') }
+
       before do
         post :create, user: user_data
       end
@@ -11,11 +12,15 @@ RSpec.describe Api::V1::RegistrationsController, type: :controller do
       it { expect(response).to have_http_status(:created) }
 
       it 'creates a new entry' do
-        expect(User.last.name).to eq('Newbie')
+        result = User.last.name
+
+        expect(result).to eq('Newbie')
       end
 
       it 'new user is active' do
-        expect(User.last).to be_active
+        result = User.last
+
+        expect(result).to be_active
       end
 
       context 'priest' do
@@ -26,7 +31,9 @@ RSpec.describe Api::V1::RegistrationsController, type: :controller do
         it { expect(response).to have_http_status(:created) }
 
         it 'new priest user is inactive' do
-          expect(User.last).not_to be_active
+          result = User.last
+
+          expect(result).not_to be_active
         end
       end
     end
