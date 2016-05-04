6eb0c5167f83dbfeb09e9de1e85aff648f7ecf2b
Oleg Sulyanov <oleg@sulyanov.com>
2016-05-04 16:27:30 +0700

    Add tests for CleanupOutdatedMessagesJob

---
+++ spec/jobs/cleanup_outdated_messages_job_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe CleanupOutdatedMessagesJob, type: :job do
+  include ActiveJob::TestHelper
+
+  subject(:job) { described_class.perform_later }
+
+  it 'queues the job' do
+    expect { job }
+      .to change(ActiveJob::Base.queue_adapter.enqueued_jobs, :size).by(1)
+  end
+
+  it 'is in default queue' do
+    expect(described_class.new.queue_name).to eq('default')
+  end
+
+  it 'executes perform' do
+    expect(CleanupOutdatedMessagesService)
+      .to receive_message_chain(:new, :perform)
+
+    perform_enqueued_jobs { job }
+  end
+end
