d5874ccae17c4d849684fd9c82ae8c6e75e28aef
Oleg Sulyanov <oleg@sulyanov.com>
2016-05-10 14:54:02 +0700

    Document helpers/application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,13 @@
 module ApplicationHelper
+  # Returns an array of abbreviated to two letters week days in French
+  # starting from monday.
   def french_weekdays
     %w(Di Lu Ma Me Je Ve Sa)
   end
 
+  # Returns current time in YTC with date 01.01.2000
+  #
+  #   If current time 07:30 (UTC) # => 2000-01-01 07:30:00 UTC
   def time_now
     ctime = Time.now.getlocal
     Time.new(2000, 01, 01, ctime.strftime('%H'),
