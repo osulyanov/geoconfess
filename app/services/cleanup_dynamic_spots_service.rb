15859a4aaa40d3e7ff7643cb468477d4bc1b00be
Oleg Sulyanov <oleg@sulyanov.com>
2016-05-10 16:56:59 +0700

    Document the code of cleanup_dynamic_spots_service.rb

--- app/services/cleanup_dynamic_spots_service.rb
+++ app/services/cleanup_dynamic_spots_service.rb
@@ -3,10 +3,12 @@ class CleanupDynamicSpotsService
     @outdated_spots = spots_to_remove
   end
 
+  # Destroys all +outdated_spots+.
   def perform
     @outdated_spots.destroy_all
   end
 
+  # Returns outdated spots (<tt>Spot.outdated</tt>) which should be removed.
   def spots_to_remove
     Spot.outdated
   end
