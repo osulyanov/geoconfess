38ba8179bed66cf34f567a9e6e7475ac955770bf
Oleg Sulyanov <oleg@sulyanov.com>
2016-06-07 18:05:48 +0700

    Improve users select process within Admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,7 +24,7 @@ class User < ActiveRecord::Base
   validates :os, presence: true, if: 'push_token.present?'
 
   def self.collection_for_admin
-    all.map { |u| ["#{u.id}. #{u.display_name}", u.id] }
+    order(:id).map { |u| ["#{u.id}. #{u.display_name}", u.id] }
   end
 
   def display_name
